6ba21456f0843dee30db846704aada45ba060d36
y <yannis.filopoulos@gmail.com>
2018-11-01 11:58:36 +0200

    Changed the menu snippet

--- site/config/config.php
+++ site/config/config.php
@@ -16,7 +16,7 @@ for more information: http://getkirby.com/license
 */
 
 c::set('license', 'put your license key here');
-
+c::set('panel.install', true);
 /*
 
 ---------------------------------------
